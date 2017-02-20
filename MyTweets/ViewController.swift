0c48f81e4cb733f9d644dfcba6338f878e6fe386
bwong <bwong@sfs-wifi-dhcp-10-143-214-131.wifi.sfsu.edu>
2017-02-19 17:18:23 -0800

    Initial Commit

---
+++ MyTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTweets
+//
+//  Created by bwong on 2/19/17.
+//  Copyright © 2017 mwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
